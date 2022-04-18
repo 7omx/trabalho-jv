35939d5e115212170b420e320196a34530b5b6c3
7omx <tomazj995@gmail.com>
2022-04-18 19:00:23 -0300

    first commit

---
+++ trabalho java 1/src/trabalhoq2.java
@@ -0,0 +1,20 @@
+
+public class trabalhoq2 {
+
+	public static void main(String[] args) {
+		double a = 3.6;
+		double b = 7.8;
+		double c = 9.2;
+		double PI = 3.14159 ;
+		double valort = (a * c) / 2;
+		double valorc = PI * (c*c);
+		double valortr = (a + b) * c / 2 ; 
+		double valorq = b * b ;
+		double valorr = a * b;
+		System.out.println("area do triangulo: " + valort);
+		System.out.println("area do circulo: " + valorc);
+		System.out.println("area do trapezio: " + valortr);
+		System.out.println("area do quadrado: " + valorq);
+		System.out.println("area do retăngulo: " + valorr);
+	}
+}
---
+++ trabalho java 1/trabalhoq1.java
@@ -0,0 +1,10 @@
+
+public class trabalhoq1{
+
+	public static void main(String[]args) {
+	int r = 7;
+	double PI = 3.1416; 
+	double a = PI *(r*r);
+		System.out.println("Valor da area é: "+ a);
+	}	
+}
